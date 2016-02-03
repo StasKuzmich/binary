da02a885a25c508a2db588e63911ac8fb549819d
StasKuzmich <kuzmichstas1992@gmail.com>
2016-02-03 12:51:41 +0300

    p1

---
+++ binary-tree.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var p = class Node {
+	constructor(data, left, right) {
+		this.data = data || null;
+		this.left = left || null;
+		this.right = right || null;
+	}
+}
+
+class BinaryTree {
+	constructor() {
+		this.root = null;
+	}
+
+	insert(data) {
+		var n = new Node(data, null, null);
+		var present_position = this.root;
+		var parent;
+
+		if (this.root === null){
+			this.root = n;
+		} else { 
+			while (true){
+				parent = present_position;
+				if (data < present_position.data) {
+					present_position = present_position.left;
+					if (present_position === null) {
+						parent.left = n;
+						break;
+					}
+				} else {
+					present_position = present_position.right;
+					if(present_position === null){
+						parent.right = n;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	contains(data) {
+		var current = this.root;
+		while (current.data !== data){
+			if (data < current.data) {
+				current = current.left;
+			} else {
+				current = current.right;
+			}
+			if (current === null) {
+				return false;
+				break;
+			}
+		}
+		return true;
+	}
+
+	remove(data) {
+
+	}
+
+	size() {
+
+	}
+
+	isEmpty() {
+
+	}
+}
