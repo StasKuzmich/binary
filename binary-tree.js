88d4b9245cfb4b798c2e61a795cb1e34b27db903
StasKuzmich <kuzmichstas1992@gmail.com>
2016-02-03 13:16:08 +0300

    p2

--- binary-tree.js
+++ binary-tree.js
@@ -57,6 +57,69 @@ class BinaryTree {
 	}
 
 	remove(data) {
+		var found = false,
+			parent = null,
+			current = this.root,
+			childCount,
+			replacement,
+			replacementParent;
+
+        while(!found && current){
+        
+            if (data < current.data){
+                parent = current;
+                current = current.left;
+            } else if (data > current.data){
+                parent = current;
+                current = current.right;
+            } else if (data === current.data){
+                found = true;
+            } else{
+            	break;
+            }
+        }
+
+		if(found){
+
+			childCount = (current.left !== null ? 1 : 0) + (current.right !== null ? 1 : 0);
+
+			// Р—РЅР°С‡РµРЅРёРµ РІ РєРѕСЂРЅРµ
+			if (current === this.root){
+				// РќРµС‚Сѓ РґРµС‚РµР№
+				if (childCount === 0){
+					this.root = null;
+				// 1 children
+				} else if (childCount === 1){
+					this.root = (current.right === null ? current.left : current.right);
+				// 2 childrens
+				} else {
+				
+				}
+			// Р—РЅР°С‡РµРЅРёРµ РЅРµ РІ РєРѕСЂРЅРµ
+			} else {
+				// РќРµС‚Сѓ РґРµС‚РµР№
+				if (childCount === 0){
+					// РµСЃР»Рё Р·РЅР°С‡РµРЅРёРµ РјРµРЅСЊС€Рµ СЂРѕРґРёС‚РµР»СЏ СѓРґР°Р»СЏРµРј Р»РµРІС‹Р№ СЌР»РµРјРµРЅС‚
+					if (current.data < parent.data){
+						parent.left = null;
+					// РµСЃР»Рё Р·РЅР°С‡РµРЅРёРµ Р±РѕР»СЊС€Рµ СЂРѕРґРёС‚РµР»СЏ СѓРґР°Р»СЏРµРј РїСЂР°РІС‹Р№ СЌР»РµРјРµРЅС‚
+					} else {
+						parent.right = null;
+					}
+				// Р•СЃС‚СЊ 1 children
+				} else if (childCount === 1) {
+					if (current.data < parent.data){
+						parent.left = (current.left === null ? current.right : current.left);
+					} else {
+						parent.right = (current.left === null ? current.right : current.left);
+					}
+				// РµСЃР»Рё 2 childrens
+				} else {
+					
+				}
+			}
+
+		}
 
 	}
 
@@ -65,6 +128,6 @@ class BinaryTree {
 	}
 
 	isEmpty() {
-
+		return this.root === null;
 	}
 }
